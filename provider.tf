274a54c8ab17a3326ece2d8c36ec810ad634d56f
oczyking <mat-loe@outlook.com>
2022-10-18 08:10:00 +0000

    terraform and github actions

--- provider.tf
+++ provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "<student-navn>/apprunner-actions.state"
+    key    = "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/apprunner-actions.state"
     region = "eu-north-1"
-  }
+}
 }
 
